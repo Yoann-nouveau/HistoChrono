bc70a8fc517adb26606dbc079057fbe727690bf3
Kevin Lafonta <kev.lafonta@gmail.com>
2023-06-06 15:41:05 +0200

    controller

---
+++ app/controllers/personalities_controller.rb
@@ -0,0 +1,29 @@
+class PersonalitiesController < ApplicationController
+  def new
+    @personalities = Personality.new
+  end
+
+  def create
+    @period = Period.find(params[:period_id])
+    @personalities = Personality.new(personalities_params)
+    @personalities.user = current_user
+    @personalities.period = @period
+    if @personalities.save
+      redirect_to periods_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def personalities_params
+    params.require(:personality).permit(
+      :fullname,
+      :birthdate,
+      :deathdate,
+      :description,
+      :wikipedia_url
+    )
+  end
+end
