99a5d0f1f4eaab63691ec848ce8fd098ecd37295
Yoann Nouveau <yoann_nouveau@hotmail.fr>
2023-06-06 17:11:21 +0200

    bug js corrigé

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,9 +4,6 @@
 
 import { application } from "./application"
 
-import BannerController from "./banner_controller"
-application.register("banner", BannerController)
-
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
