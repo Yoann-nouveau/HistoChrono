8b046d2666bc8b137a79bc0941e975ba2c308d80
Yoann Nouveau <yoann_nouveau@hotmail.fr>
2023-06-06 15:54:55 +0200

    models modifiés pour changer les dates en jour/mois/année

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,8 +3,8 @@ class Event < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true
-  validates :start_date, presence: true
-  validates :end_date, presence: true
+  validates :start_year, presence: true
+  validates :end_year, presence: true
   validates :location, presence: true
   validates :description, presence: true
   validates :wikipedia_url, presence: true
