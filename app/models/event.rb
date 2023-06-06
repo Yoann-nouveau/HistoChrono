44a6692ab6c084dc8dda1f0e33862440350fcfe6
Alexis Bourieau <alexis.bourieau@gmail.com>
2023-06-06 14:47:48 +0200

    add geocoder display map

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,10 +2,12 @@ class Event < ApplicationRecord
   belongs_to :period
   belongs_to :user
 
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_address?
+
   validates :start_date, presence: true
   validates :end_date, presence: true
   validates :location, presence: true
   validates :description, presence: true
   validates :wikipedia_url, presence: true
-
 end
--- app/models/monument.rb
+++ app/models/monument.rb
@@ -2,6 +2,9 @@ class Monument < ApplicationRecord
   belongs_to :period
   belongs_to :user
 
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_address?
+
   valdiates :name, presence: true
   valdiates :location, presence: true
   valdiates :description , presence: true
