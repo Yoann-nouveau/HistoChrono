4390e449ded0b518248c078ff50e415eb1131ebb
Alexis Bourieau <alexis.bourieau@gmail.com>
2023-06-07 14:47:44 +0200

    affichage d'un polygon

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -16,6 +16,7 @@ export default class extends Controller {
       style: "mapbox://styles/alexmcfly/clikchnpc00en01qvd5kjhaz3"
     })
 
+    this.#addPolygonToMap()
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
   }
@@ -40,6 +41,40 @@ export default class extends Controller {
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.markerseventValue.forEach(markerevent => bounds.extend([ markerevent.lng, markerevent.lat ]))
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+  }
+
+  #addPolygonToMap () {
+    this.map.on('load', () => {
+      // Add a data source containing GeoJSON data.
+      this.map.addSource('polygon', {
+          'type': 'geojson',
+          'data': './map.geojson'
+      });
+
+      // Add a new layer to visualize the polygon.
+      this.map.addLayer({
+          'id': 'main',
+          'type': 'fill',
+          'source': 'polygon', // reference the data source
+          'layout': {},
+          'paint': {
+              'fill-color': '#0080ff', // blue color fill
+              'fill-opacity': 0.5
+          }
+      });
+      // Add a black outline around the polygon.
+      this.map.addLayer({
+          'id': 'outline',
+          'type': 'line',
+          'source': 'polygon',
+          'layout': {},
+          'paint': {
+              'line-color': '#000',
+              'line-width': 3
+          }
+      });
+  });
   }
 }
