e6c0822156285a3e2ec10f9367ebe0cc6ff7200a
Kevin Lafonta <kev.lafonta@gmail.com>
2023-06-12 14:53:48 +0200

    auto scroll

--- app/javascript/controllers/scroll_controller.js
+++ app/javascript/controllers/scroll_controller.js
@@ -1,14 +1,10 @@
 import { Controller } from "@hotwired/stimulus"
 
-static targets = [ "scroll-target" ]
-
 // Connects to data-controller="scroll"
 export default class extends Controller {
+
   connect() {
-
-    console.log("hello");
-    this.element.scroll(0, 1000);
-
+    const target = document.querySelector(".circle.active")
+    target.scrollIntoView();
   }
-
 }
