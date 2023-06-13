30dd1362d2e014ff30e4b78c25025b6e7640dd9b
Yoann Nouveau <yoann_nouveau@hotmail.fr>
2023-06-13 14:45:25 +0200

    section vos marqueurs finalisés

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'redirection', to: 'periods#redirection'
   devise_for :users
   root to: "pages#home"
+  resources :markers, only: [:index]
   resources :approvals, only: [:index, :create, :update]
   resources :periods, only: [:show] do
     resources :monuments, only: [:create]
